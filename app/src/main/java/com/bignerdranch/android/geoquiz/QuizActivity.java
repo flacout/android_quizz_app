9884dead30bc4e27a4a202357bec91a6e5be36dc
flacout <fabricelacout@gmail.com>
2018-03-21 20:27:59 -0700

    retreive challenge2

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,7 +1,5 @@
 package com.bignerdranch.android.geoquiz;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,30 +7,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-public class QuizActivity extends AppCompatActivity{
-    private static final String TAG = "QuizActivity";
-    private static final String KEY_INDEX = "index";
-    private static final int REQUEST_CODE_CHEAT = 0;
-
-
+public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
-    private Button mCheatButton;
+    private ImageButton mPrevButton;
+    private ImageButton mNextButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -43,19 +26,31 @@ public class QuizActivity extends AppCompatActivity{
             new Question(R.string.question_asia, true),
     };
     private int mCurrentIndex = 0;
-    private boolean mIsCheater;
-    private RequestQueue queue;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
 
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkAnswer(true);
+                Log.d("correct", "correct answer");
+            }
+        });
 
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(new View.OnClickListener() {
@@ -64,105 +59,27 @@ public class QuizActivity extends AppCompatActivity{
                 checkAnswer(false);
             }
         });
-/*
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+        mPrevButton = (ImageButton) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkAnswer(true);
-            }
-        });*/
-
-        mNextButton = (Button) findViewById(R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
-                mIsCheater = false;
+                // keep index positive
+                mCurrentIndex = (mCurrentIndex -1 +mQuestionBank.length) % mQuestionBank.length;
+                Log.d("index", Integer.toString(mCurrentIndex));
                 updateQuestion();
             }
         });
 
-        mCheatButton = (Button)findViewById(R.id.cheat_button);
-        mCheatButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start CheatActivity
-                //Intent i = new Intent(QuizActivity.this, CheatActivity.class);
-                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-                Intent i = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
-                //startActivity(i);
-                startActivityForResult(i, REQUEST_CODE_CHEAT);
-            }
-        });
-
-
-
-        if (savedInstanceState != null){
-            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
-        }
-        updateQuestion();
-
-        // INTERNET HTTP REQUEST
-        queue = Volley.newRequestQueue(this);
-        String url ="http://fabricelacout.pythonanywhere.com/";
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 1000 characters of the response string.
-                        Log.d(TAG, response.substring(0, 1000));
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d(TAG, "That didn't work!");
-                }
-        });
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String url = "http://fabricelacout.pythonanywhere.com/andro";
-                JsonObjectRequest jsonO = new JsonObjectRequest(
-                        Request.Method.GET, url, null,
-                        new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try{
-                            Log.d(TAG, "JSON_HELLO"+ response.get("hello"));
-                        }
-                        catch (JSONException e){;}
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d(TAG, "error in my json");
-                    }
-                });
-                queue.add(jsonO);
-
+                mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
+                updateQuestion();
             }
         });
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != Activity.RESULT_OK){
-            return;
-        }
-        if (requestCode == REQUEST_CODE_CHEAT){
-            if (data == null){
-                return;
-            }
-            mIsCheater = CheatActivity.wasAnswerShown(data);
-        }
+        updateQuestion();
     }
 
 
@@ -174,53 +91,13 @@ public class QuizActivity extends AppCompatActivity{
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
         int messageResId = 0;
-
-        if (mIsCheater){
-            messageResId = R.string.judgment_toast;
+        if (userPressedTrue==answerIsTrue){
+            messageResId = R.string.correct_toast;
         }
-        else {
-            if (userPressedTrue==answerIsTrue){
-                messageResId = R.string.correct_toast;
-            }
-            else{
-                messageResId = R.string.incorrect_toast;
-            }
+        else{
+            messageResId = R.string.incorrect_toast;
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState){
-        super.onSaveInstanceState(savedInstanceState);
-        Log.i(TAG, "onSaveInstanceState");
-        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
-    }
-
-    @Override
-    public void onStart(){
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-    @Override
-    public void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
